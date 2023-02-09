66775ec6652b145c8cd248a0f0b98f9c8f44d1a6
Vadim Sultanov <vadim.sultanov@quadcode.com>
2023-02-09 20:24:41 +0200

    add host name

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +11,19 @@ import (
 func main() {
 	fmt.Println("Run web service")
 
+	hostName, err := os.Hostname()
+	if err != nil {
+		return
+	}
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+			"host":    hostName,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err != nil {
+		return
+	}
 }
